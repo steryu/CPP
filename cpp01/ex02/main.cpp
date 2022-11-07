69849a529c18349b7cfdd22e54ee8d6a1a557d94
Ster Van asselt <svan-ass@f1r6s16.codam.nl>
2022-11-07 15:00:24 +0100

    added cpp01 ex02

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,12 +6,22 @@
 /*   By: svan-ass <svan-ass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 14:08:07 by svan-ass          #+#    #+#             */
-/*   Updated: 2022/11/04 14:09:22 by svan-ass         ###   ########.fr       */
+/*   Updated: 2022/11/07 14:41:27 by svan-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int main(void){
-	std::string = "HI THIS IS BRAIN" << std::endl;
+	std::string str = "HI THIS IS BRAIN";
+	std::string* stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << "\nThe value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
 }
