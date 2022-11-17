2986b9fff9b272b916d788366989b4f6ea964e03
Ster Van asselt <svan-ass@f1r6s16.codam.nl>
2022-11-17 12:45:25 +0100

    done

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: svan-ass <svan-ass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 15:38:34 by svan-ass          #+#    #+#             */
-/*   Updated: 2022/11/16 15:22:46 by svan-ass         ###   ########.fr       */
+/*   Updated: 2022/11/17 12:45:00 by svan-ass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int argc, char **argv){
 	if (argc != 2){
-		std::cout << "only one parameter please: DEBUG, INFO, WARNING of ERROR." << std::endl;
+		std::cout << "only one parameter please: DEBUG, INFO, WARNING or ERROR." << std::endl;
 		exit(EXIT_SUCCESS);
 	}
 	Harl level;
